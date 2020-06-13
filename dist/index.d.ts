0c22e4b85424a58c230cf585edfcceeb6d5f0901
Roman Mashta <roman.mashta@gmail.com>
2020-06-13 03:05:13 +0300

    Remove build files

--- dist/index.d.ts
+++
@@ -1,4 +0,0 @@
-import ts from 'typescript';
-import { MetaInfo } from '@spcy/lib.core.reflection';
-export declare const generateMetaInfoForFiles: (files: string[], options: ts.CompilerOptions) => MetaInfo;
-export declare const generateMetaInfoForFile: (file: string, options?: ts.CompilerOptions) => MetaInfo;
