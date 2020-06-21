4821e295b652954b62a236fcf3848646e1d03401
Roman Mashta <roman.mashta@gmail.com>
2020-06-22 00:03:23 +0300

    Schema repository added

--- src/templates/module.ts
+++ src/templates/module.ts
@@ -1,7 +1,8 @@
-export const ModuleTemplate = `import { TypeInfo, Module } from '@spcy/lib.core.reflection';
+export const ModuleTemplate = `import { TypeInfo, Module, SchemaRepository } from '@spcy/lib.core.reflection';
 
 {{#each module.$defs}}
 export const {{@key}}Schema: TypeInfo = {{stringify .}};
+SchemaRepository.register({{@key}}Schema);
 
 {{/each}}
 
