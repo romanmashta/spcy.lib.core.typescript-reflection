816da95456e86a79bf921991b3d4140ff3cb922d
Roman Mashta <roman.mashta@gmail.com>
2020-07-07 02:06:26 +0300

    Template updates

--- src/templates/module.ts
+++ src/templates/module.ts
@@ -1,15 +1,19 @@
-export const ModuleTemplate = `import * as r from '@spcy/lib.core.reflection';
+export const ModuleTemplate = `
+import * as r from '@spcy/lib.core.reflection';
 {{#if isEmpty}}
 {{else}}
 import * as m from './{{moduleFileName}}';
 {{/if}}
 {{#each exports}}
-import { {{importName}}Module as {{aliasName}}Module } from '{{fileName}}';
+import { {{importName}}Module as {{aliasName}}Module, Types as {{aliasName}}Types } from '{{fileName}}';
 {{/each}}
 
 {{#each module.$defs}}
-export const {{@key}}Type: r.TypeInfo & r.Prototype<m.{{@key}}> = {{stringify .}};
-
+const {{@key}}Type: r.TypeInfo = {{stringify .}};
+const {{@key}}: r.Prototype<m.{{@key}}> = { 
+  id: {{@key}}Type.$id,
+  typeInfo: {{@key}}Type,
+}
 {{/each}}
 
 export const {{moduleName}}Module: r.Module = {
@@ -24,4 +28,12 @@ export const {{moduleName}}Module: r.Module = {
   }
 };
 
+export const Types = {
+{{#each module.$defs}}
+    {{@key}},
+{{/each}}
+{{#each exports}}
+    ...{{aliasName}}Types,
+{{/each}}
+};
 `;
